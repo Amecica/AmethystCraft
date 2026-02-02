5d8f837be9680324c5157b00de94e2792f5f93b0
jgj52 <email@jgj52.hu>
2026-02-02 19:59:04 +0100

    main

--- src/main/java/hu/jgj52/amethystCraft/AmethystCraft.java
+++ src/main/java/hu/jgj52/amethystCraft/AmethystCraft.java
@@ -4,10 +4,15 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AmethystCraft extends JavaPlugin {
 
+    public static AmethystCraft plugin;
+
     @Override
     public void onEnable() {
         // Plugin startup logic
+        getConfig().options().copyDefaults(true);
+        saveConfig();
 
+        plugin = this;
     }
 
     @Override
