f345de34da3d30f8163f8feb91d7f3fe78657b75
jgj52 <email@jgj52.hu>
2026-02-02 19:50:21 +0100

    initial commit

---
+++ src/main/java/hu/jgj52/amethystCraft/AmethystCraft.java
@@ -0,0 +1,17 @@
+package hu.jgj52.amethystCraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AmethystCraft extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
